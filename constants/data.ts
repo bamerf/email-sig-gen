24c8cc03f47fe989e864e3eebb723adf07710b73
Bamdad Erfanian <erfanianbamdad@gmail.com>
2023-05-26 17:16:09 +0400

    add data

---
+++ constants/data.ts
@@ -0,0 +1,73 @@
+export const DATA = {
+  fullName: {
+    label: 'Full Name',
+    placeholder: 'Jack Smith',
+    data: 'Bamdad Erfanian',
+  },
+  role: {
+    label: 'Role',
+    placeholder: 'Senior Software Engineer',
+    data: 'Senior Frontend Engineer',
+  },
+  company: {
+    label: 'Company',
+    placeholder: 'Company, Inc.',
+    data: 'Corellium',
+  },
+  companyWebsite: {
+    label: 'Company Website',
+    placeholder: 'https://company.com',
+    data: 'https://corellium.com',
+  },
+  companyAddress: {
+    label: 'Company Address',
+    placeholder: '123 Main St, City, State, Zip',
+    data: 'Delray Beach, FL',
+  },
+  email: {
+    label: 'Email',
+    placeholder: 'me@company.com',
+    data: 'bam@corellium.com',
+  },
+  phoneNumber: {
+    label: 'Phone Number',
+    placeholder: '+1 234 567 890',
+    data: '+1 234 567 890',
+  },
+  mobileNumber: {
+    label: 'Mobile Number',
+    placeholder: '+1 234 567 890',
+    data: '+1 234 567 890',
+  },
+  bookingLink: {
+    label: 'Booking Link',
+    placeholder: 'https://example.com/booking',
+    data: 'https://example.com/booking',
+  },
+  logoUrl: {
+    label: 'Logo URL',
+    placeholder: 'https://example.com/logo.png',
+    data: 'https://source.unsplash.com/random',
+  },
+  footer: {
+    label: 'Footer',
+    placeholder: 'Start typing your footer here...',
+    data: 'CONFIDENTIALITY NOTICE — This email with all attachment(s) is solely for the use of the individual or entity to which it was intended. Unless otherwise indicated, it contains information that is confidential, privileged and/or exempt from disclosure under applicable law. If you are not the intended recipient, any disclosure, copying, distribution, or action taken based on this email is strictly prohibited. If you have received this email in error, please notify the sender of the error and delete the email. Thank you.',
+  },
+  fontStack: {
+    label: 'Font Stack',
+    placeholder:
+      'Inter, system-ui, -apple-system, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, Liberation Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+    data: 'Inter, system-ui, -apple-system, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, Liberation Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+  },
+  primaryColor: {
+    label: 'Primary Color',
+    placeholder: '#000000',
+    data: '#000000',
+  },
+  otherLinks: {
+    label: 'Other Links',
+    placeholder: 'https://linkedin.com/your-profile',
+    data: 'https://bamerf.com',
+  },
+};
