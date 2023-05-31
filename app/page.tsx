a54dc3c8b73506ad01a1184619ef4ce77fac1455
Bamdad Erfanian <erfanianbamdad@gmail.com>
2023-05-31 17:32:03 +0400

    add syntax highlighting

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,37 @@
 'use client';
 
 import { useState } from 'react';
-import SignatureOutput from '@/components/SignatureOutput';
-import { render } from '@react-email/render';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {
-  docco,
-  atelierHeathDark,
-} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import ReactDOMServer from 'react-dom/server';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-import FieldsSection from '@/components/FieldsSection';
 import { DATA } from '@/constants/data';
+import SignatureOutput from '@/components/SignatureOutput';
+import FieldsSection from '@/components/FieldsSection';
 
 export default function Home() {
   const [data, setData] = useState(DATA);
 
-  // const html = render(<ReactEmail {...data} />).replace(/<!DOCTYPE[^>]*>/i, '');
+  const html = ReactDOMServer.renderToString(<SignatureOutput {...data} />);
 
   return (
     <div className="flex h-full gap-4">
       <FieldsSection data={data} setData={setData} />
       <div className="flex flex-col w-full h-full gap-4">
         <div className="relative flex p-4 overflow-auto text-sm text-white border rounded bg-stone-950 border-stone-700 h-1/2">
-          {/* {`${html}`} */}
-          {/* <SyntaxHighlighter
-            style={atelierHeathDark}
+          <SyntaxHighlighter
+            style={vscDarkPlus}
+            customStyle={{
+              padding: 0,
+              overflowX: 'hidden',
+            }}
+            useInlineStyles
             language="html"
             wrapLongLines
+            className="syntax"
           >
-            
-          </SyntaxHighlighter> */}
+            {`${html}`}
+          </SyntaxHighlighter>
         </div>
         <div className="p-6 bg-white border rounded h-1/2 border-neutral-200">
           <SignatureOutput {...data} />
